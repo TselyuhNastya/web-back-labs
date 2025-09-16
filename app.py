86f94b1eb4c8abcf0c908e61d7c62a42e28dc3fd
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-09-16 06:29:06 +0300

    Коды оветов HTTP

--- app.py
+++ app.py
@@ -1,10 +1,114 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 import datetime
+
 app = Flask(__name__)
+
+@app.errorhandler(400)
+def bad_request(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>400 Bad Request</title>
+    </head>
+    <body>
+        <h1>400 Bad Request</h1>
+        <p>Сервер не может обработать запрос из-за неверного синтаксиса.</p>
+        <p>Проверьте правильность введенных данных и повторите попытку.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>
+''', 400
+
+@app.errorhandler(401)
+def unauthorized(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>401 Unauthorized</title>
+    </head>
+    <body>
+        <h1>401 Unauthorized</h1>
+        <p>Для доступа к запрашиваемому ресурсу требуется аутентификация.</p>
+        <p>Пожалуйста, предоставьте действительные учетные данные для продолжения.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>
+''', 401
+
+@app.errorhandler(403)
+def forbidden(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>403 Forbidden</title>
+    </head>
+    <body>
+        <h1>403 Forbidden</h1>
+        <p>У вас нет прав доступа к запрашиваемому ресурсу.</p>
+        <p>Сервер понял запрос, но отказывается его авторизовать.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>
+''', 403
 
 @app.errorhandler(404)
 def not_found(err):
     return "Нет такой информации", 404
+
+@app.errorhandler(405)
+def method_not_allowed(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>405 Method Not Allowed</title>
+    </head>
+    <body>
+        <h1>405 Method Not Allowed</h1>
+        <p>Запрашиваемый метод не поддерживается для указанного ресурса.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>
+''', 405
+
+@app.errorhandler(418)
+def im_a_teapot(err):
+    return '''
+<!doctype html>
+<html>
+    <head>
+        <title>418 I'm a teapot</title>
+    </head>
+    <body>
+        <h1>418 I'm a teapotк</h1>
+        <p>Сервер отказывается заваривать кофе, потому что он является чайником.</p>
+        <a href="/">На главную</a>
+    </body>
+</html>
+''', 418
+
+@app.route("/test/400")
+def test_400():
+    abort(400)
+
+@app.route("/test/401")
+def test_401():
+    abort(401)
+
+@app.route("/test/403")
+def test_403():
+    abort(403)
+
+@app.route("/test/405")
+def test_405():
+    abort(405)
+
+@app.route("/test/418")
+def test_418():
+    abort(418)
 
 @app.route("/")
 @app.route("/index")
