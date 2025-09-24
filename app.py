28a3e2af4b021210144999971278cd75fb03aaa1
Анастасия Целюх <annashamtyuk@mail.ru>
2025-09-24 17:14:33 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -394,5 +394,18 @@ def flowers(flower_id):
     else:
         return "цветок:" + flower_list[flower_id]
 
-
+@app.route('/lab2/add_flower/<name>')
+def add_flower(name):
+    flower_list.append(name)
+    return f'''
+<!doctype html>
+<html>
+    <body>
+    <h1>Добавлен новый цветок</h1>
+    <p>Название нового цветка: {name}</p>
+    <p>Всего цветов: {len(flower_list)}</p>
+    <p>Полный список: {flower_list}</p>
+    </body>
+</html>
+'''
 
