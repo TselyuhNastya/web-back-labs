3b0d82593b3e33824df63aa55dd234fe007c8111
Анастасия Целюх <annashamtyuk@mail.ru>
2025-09-24 17:07:46 +0700

    Вывод цветка

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, request, redirect
+from flask import Flask, url_for, request, redirect, abort
 from datetime import datetime
 from werkzeug.exceptions import HTTPException
 
@@ -385,4 +385,14 @@ def a():
 @app.route("/lab2/a/")
 def a2():
     return 'со слешом'
+
+flower_list = ['роза', 'тюльпан', 'незабудка', 'ромашка']
+@app.route('/lab2/flowers/<int:flower_id>')
+def flowers(flower_id):
+    if flower_id >= len (flower_list):
+        abort(404)
+    else:
+        return "цветок:" + flower_list[flower_id]
+
+
 
