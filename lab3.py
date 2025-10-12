fbbd9bd469f85f9ee2e57d7e65a0010bae3da988
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-10-12 14:07:42 +0300

    очистка всех кук

--- lab3.py
+++ lab3.py
@@ -111,6 +111,16 @@ def settings():
     return resp
 
 
+@lab3.route("/lab3/settings/reset")
+def reset_settings():
+    resp = make_response(redirect('/lab3/settings'))
+    
+    resp.delete_cookie('color')
+    resp.delete_cookie('bg_color')
+    resp.delete_cookie('font_size')
+    resp.delete_cookie('line_height')
+    return resp
+
 
 @lab3.route('/lab3/ticket')
 def ticket_form():
