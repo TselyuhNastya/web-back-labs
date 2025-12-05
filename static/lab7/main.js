ad1a23713daa9bc8feba379b0005990d479db4da
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-12-05 09:06:43 +0300

    Редактирование фильмов

--- static/lab7/main.js
+++ static/lab7/main.js
@@ -21,6 +21,9 @@ function fillFilmList() {
                 
                 let editButton = document.createElement('button');
                 editButton.innerText = 'редактировать';
+                editButton.onclick = function() {
+                    editFilm(i);
+                };
                 
                 let delButton = document.createElement('button');
                 delButton.innerText = 'удалить';
@@ -76,6 +79,7 @@ function addFilm() {
 }
 
 function sendFilm() {
+    const id = document.getElementById('id').value;
     const film = {
         title: document.getElementById('title').value,
         title_ru: document.getElementById('title_ru').value,
@@ -83,8 +87,8 @@ function sendFilm() {
         description: document.getElementById('description').value
     };
 
-    const url = '/lab7/rest-api/films/';
-    const method = 'POST';
+    const url = `/lab7/rest-api/films/${id}`;
+    const method = id === '' ? 'POST' : 'PUT';
     
     fetch(url, {
         method: method,
@@ -96,3 +100,18 @@ function sendFilm() {
         hideModal();
     });
 }
+
+function editFilm(id) {
+    fetch(`/lab7/rest-api/films/${id}`)
+        .then(function (data) {
+            return data.json();
+        })
+        .then(function (film) {
+            document.getElementById('id').value = id;
+            document.getElementById('title').value = film.title;
+            document.getElementById('title_ru').value = film.title_ru;
+            document.getElementById('year').value = film.year;
+            document.getElementById('description').value = film.description;
+            showModal();
+        });
+    }
