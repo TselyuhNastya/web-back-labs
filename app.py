9d20986ed8d4b521260affaa07187a6a494337aa
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-12-10 16:07:39 +0300

    самостоятельные задания

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ login_manager.init_app(app)
 def load_users(login_id):
     return users.query.get(int(login_id))
 
-app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно-секретный секрет')
-app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'sqlite')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'секретно секретный-секрет').encode('utf-8')
+app.config['DB_TYPE'] = os.getenv('DB_TYPE', 'postgres')
 
 if app.config['DB_TYPE'] == 'postgres':
     db_name = 'nastya_tseluh_orm'
