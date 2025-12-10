89befcafb1496fcf38c8d0a65b814b5ce4d79cb3
Tselyuh Nastya <annashamtyuk@mail.ru>
2025-12-10 13:30:48 +0300

    заготовка к 8-ой лабораторной

---
+++ lab8.py
@@ -0,0 +1,38 @@
+from flask import Blueprint, jsonify, abort, request, render_template, make_response, redirect, session, current_app
+from datetime import datetime
+from werkzeug.security import check_password_hash, generate_password_hash
+import sqlite3
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import os
+from os import path
+
+lab8 = Blueprint('lab8', __name__)  
+
+def db_connect():
+    if current_app.config['DB_TYPE'] == 'postgres':
+        conn = psycopg2.connect(
+            host='127.0.0.1',
+            database='nastya_tselukh_knowledge_base',
+            user='nastya_tselukh_knowledge_base',
+            password='123'
+        )
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+    else:
+        dir_path = path.dirname(path.realpath(__file__))
+        db_path = path.join(dir_path, "database.db")
+        conn = sqlite3.connect(db_path)
+        conn.row_factory = sqlite3.Row
+        cur = conn.cursor()
+    
+    return conn, cur
+
+def db_close(conn, cur):
+    conn.commit()
+    cur.close()
+    conn.close()
+
+@lab8.route('/lab8/')
+def main():
+    login = session.get('login')
+    return render_template('lab8/lab8.html', login=login)
